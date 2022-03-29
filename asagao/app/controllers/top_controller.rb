44ca6adcaf0e4dfbd19e16e5505cbb7145d71f48
yusama125718 <yuheisan0329@gmail.com>
2022-03-29 15:59:46 +0900

    完成

--- asagao/app/controllers/top_controller.rb
+++ asagao/app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
   def index
+    reset_session
+    flash[:system] = true
   end
 
 
@@ -66,26 +68,51 @@ class TopController < ApplicationController
     case wyaku
     when 3,4,5,6,7,8
       return 1
+    when 10,11,12,13,14
+      return 3
+    else
+      return 0
     end
   end
 
   def game
+    if params[:p].length > 10
+      redirect_to "/", flash: {notice: "名前は10文字以下にしてください"}
+      return
+    elsif params[:p] == ""
+      redirect_to "/", flash: {notice: "名前を入力してください"}
+      return
+    end
     session[:roll] = 0
     session[:player] = params[:p]
+    if session[:player] == nil
+      redirect_to "/error"
+      return
+    end
     session[:pdeme] = dice
+    session[:system] = 0
     redirect_to "/game/result1"
   end
 
   def game1
+    if session[:system] != 0
+      redirect_to "/error"
+      return
+    end
     session[:pdeme] = dice
     redirect_to "/game/result1"
   end
 
   def game2
+    if session[:system] != 1
+      redirect_to "/error"
+      return
+    end
     judge(session[:pdeme])
     session[:pyakuname] = $yakuname
     session[:pyaku] = $yaku
     session[:roll] += 1
+    session[:system] = 2
     if session[:pyaku] == 1 && session[:roll] < 3
       redirect_to "/game/result20"
     else
@@ -94,38 +121,67 @@ class TopController < ApplicationController
   end
 
   def game3
+    if session[:system] != 3
+      redirect_to "/error"
+      return
+    end
     session[:cdeme] = dice
+    session[:system] = 4
     redirect_to "/game/result3"
   end
 
   def result1
+    if session[:system] != 0
+      redirect_to "/error"
+      return
+    end
     @deme = session[:pdeme]
+    session[:system] = 1
   end
 
   def result2
+    if session[:system] != 2
+      redirect_to "/error"
+      return
+    end
     @deme = session[:pdeme]
     @pyakuname = session[:pyakuname]
     @player = session[:player]
     session[:roll] = 0
+    session[:system] = 3
   end
 
   def result20
+    if session[:system] != 2
+      redirect_to "/error"
+      return
+    end
     @deme = session[:pdeme]
     @pyakuname = session[:pyakuname]
     @player = session[:player]
     @roll = session[:roll]
+    session[:system] = 0
   end
 
   def result3
+    if session[:system] != 4
+      redirect_to "/error"
+    end
     @player = session[:player]
     @deme = session[:cdeme]
+    session[:system] = 5
   end
 
   def game4
+    if session[:system] != 5
+      redirect_to "/error"
+      return
+    end
     judge(session[:cdeme])
     session[:cyakuname] = $yakuname
     session[:cyaku] = $yaku
     session[:roll] += 1
+    session[:system] = 6
     if session[:cyaku] == 1 && session[:roll] < 3
       redirect_to "/game/result40"
     else
@@ -134,36 +190,73 @@ class TopController < ApplicationController
   end
 
   def result4
+    if session[:system] != 6
+      redirect_to "/error"
+      return
+    end
     @deme = session[:cdeme]
     @cyakuname = session[:cyakuname]
     @player = session[:player]
+    session[:system] = 7
   end
 
   def result40
+    if session[:system] != 6
+      redirect_to "/error"
+      return
+    end
     @deme = session[:cdeme]
     @cyakuname = session[:cyakuname]
     @player = session[:player]
     @roll = session[:roll]
+    session[:system] = 3
   end
 
   def finish
+    if session[:system] != 7
+      redirect_to "/error"
+      return
+    end
     @player = session[:player]
     @pyakuname = session[:pyakuname]
     @pdeme = session[:pdeme]
     @cdeme = session[:cdeme]
     @cyakuname = session[:cyakuname]
-    if session[:pyaku] == 2
+    case session[:pyaku]
+    when 2
       @winner = "子(#{session[:player]})"
       @vakue = 2
+      return
+    when 9
+      @winner = "親(CPU)"
+      @value =2
+      return
+    when 15
+      @winner = "親(CPU)"
+      @value = 5
+      return
+    end
+    case session[:cyaku]
+    when 2
+      @winner = "親(CPU)"
+      @value = 2
+      return
+    when 9
+      @winner = "子(#{session[:player]})"
+      @value =2
+      return
+    when 15
+      @winner = "子(#{session[:player]})"
+      @value = 5
       return
     end
     if session[:cyaku] > session[:pyaku]
       @winner = "子(#{session[:player]})"
       @value = check(session[:cyaku])
-    else if session[:cyaku] == session[:pyaku]
+    elsif session[:cyaku] == session[:pyaku]
       @winner = "引き分け"
       @value = "-"
-    else if session[:cyaku] < session[:pyaku]
+    elsif session[:cyaku] < session[:pyaku]
       @winner = "親(CPU)"
       @value = check(session[:pyaku])
     end
