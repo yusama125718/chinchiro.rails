bd866bfb6d21061ed247c82dda08d79f5db4771f
yusama125718 <yuheisan0329@gmail.com>
2022-05-27 13:35:30 +0900

    railsのバージョン変更

--- asagao/config/application.rb
+++ asagao/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Asagao
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    # config.load_defaults 7.0
 
     config.exeptions_app = -> (env) do
       ErrorsController.action(:show).call(env)
